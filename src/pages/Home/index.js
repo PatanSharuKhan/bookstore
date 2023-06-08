cee46ebeff3b85b8275fe6558ae0a96cb283e130
patansharukhan <patansharu654321@gmail.com>
2023-06-08 23:24:35 +0530

    Added Landing section in Home Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,30 @@
 import { Component } from "react"
-import { H, P } from "./styledComponents"
+import {
+  H,
+  P,
+  Img,
+  LandingData,
+  LandingSection,
+  Button,
+} from "./styledComponents"
 
 class Home extends Component {
   render() {
     return (
       <>
-        <H>Home</H>
-        <P>Page</P>
+        <LandingSection>
+          <LandingData>
+            <H>Discover Your Favourite Books, Order Online</H>
+            <P>One of the largest e-commerse website in India</P>
+            <a href="/books">
+              <Button>Discover Books</Button>
+            </a>
+          </LandingData>
+          <Img
+            src="https://thumbs.dreamstime.com/b/stacked-closed-books-icon-cartoon-isolated-149755178.jpg"
+            alt="landing-image"
+          />
+        </LandingSection>
       </>
     )
   }
--- src/pages/Home/styledComponents.js
+++ src/pages/Home/styledComponents.js
@@ -1,9 +1,49 @@
 import styled from "styled-components"
 
+export const LandingSection = styled.div`
+  height: 80vh;
+  @media screen and (min-width: 768px) {
+    width: 80%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+
+export const LandingData = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  @media screen and (min-width: 768px) {
+    width: 50%;
+  }
+`
+
+export const Img = styled.img`
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    width: 40%;
+  }
+`
+
 export const H = styled.h1`
   color: red;
   text-align: center;
 `
+
 export const P = styled.p`
   color: blue;
+`
+export const Button = styled.button`
+  height: 40px;
+  margin-top: 20px;
+  border: 0px;
+  border-radius: 5px;
+  background-color: pink;
+  color: red;
+  font-weight: bold;
+  cursor: pointer;
 `
