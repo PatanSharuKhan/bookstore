a01bb4295a2eb0b20598a64bd12d64dca33bbcbe
patansharukhan <patansharu654321@gmail.com>
2023-06-08 23:37:32 +0530

    on success empty cart items

--- src/pages/SuccessPayment/index.js
+++ src/pages/SuccessPayment/index.js
@@ -1,12 +1,16 @@
 import { Img, Container } from "./styledComponents"
 
-const SuccessPayment = () => (
-  <Container>
-    <Img
-      src="https://momentumacademy.net/wp-content/uploads/2020/05/Paymentsuccessful21.png"
-      alt="success-payment"
-    />
-  </Container>
-)
+const SuccessPayment = () => {
+  const cartItemsKey = "cartItems"
+  localStorage.removeItem(cartItemsKey)
+  return (
+    <Container>
+      <Img
+        src="https://momentumacademy.net/wp-content/uploads/2020/05/Paymentsuccessful21.png"
+        alt="success-payment"
+      />
+    </Container>
+  )
+}
 
 export default SuccessPayment
