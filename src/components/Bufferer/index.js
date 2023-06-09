b1977ac326703d00cda0da837263bb1cc20bb851
patansharukhan <patansharu654321@gmail.com>
2023-06-09 12:17:58 +0530

    Created the bufferer feature

---
+++ src/components/Bufferer/index.js
@@ -0,0 +1,9 @@
+import { Container, H } from "./styledComponents"
+
+const Bufferer = () => (
+  <Container>
+    <H>Loading...</H>
+  </Container>
+)
+
+export default Bufferer
---
+++ src/components/Bufferer/styledComponents.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const H = styled.h1`
+  font-color: pink;
+`
